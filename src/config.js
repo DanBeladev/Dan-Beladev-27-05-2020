867a2ac290b118175747db9ae028090f1a1a4e47
Dan <danbeladev@gmail.com>
2020-05-31 14:52:05 +0300

    change http requests to https

--- src/config.js
+++ src/config.js
@@ -8,10 +8,10 @@ export const KEY = 'mo6ils7ZelTJtAoB9uK1gVgavYUIWFkU';
 
 export const LANGUAGE = 'language=en-us';
 
-export const AUTO_COMPLETE_API = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete';
-export const CURRENT_CONDITIONS_API = 'http://dataservice.accuweather.com/currentconditions/v1/';
-export const FIVE_DAY_FORECAST_API = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/';
-export const SEARCH_BY_LOCATION_KEY = 'http://dataservice.accuweather.com/locations/v1/';
+export const AUTO_COMPLETE_API = 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete';
+export const CURRENT_CONDITIONS_API = 'https://dataservice.accuweather.com/currentconditions/v1/';
+export const FIVE_DAY_FORECAST_API = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/';
+export const SEARCH_BY_LOCATION_KEY = 'https://dataservice.accuweather.com/locations/v1/';
 
 export const ICON_URL ='https://developer.accuweather.com/sites/default/files/0'
 export const ICON_SHORTER_URL ='https://developer.accuweather.com/sites/default/files/'
