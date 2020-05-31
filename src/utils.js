7b9fdd81432f30c5088c743d33ca8984d1572325
Dan <danbeladev@gmail.com>
2020-05-31 13:05:54 +0300

    handling utils

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ export const getIconUrl = (weatherIcon) => {
   const length = weatherIcon ? weatherIcon.toString().length: 0
   if(length === 0)
   {
-    return `${ICON_URL}02${ICON_EXTENSION}`;
+    return `${ICON_URL}2${ICON_EXTENSION}`;
   }
   return length > 1
     ? `${ICON_SHORTER_URL}${weatherIcon}${ICON_EXTENSION}`
